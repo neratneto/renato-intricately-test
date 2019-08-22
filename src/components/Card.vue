ce9a2b526d519958d3802d7446ac28dba2e95f87
Neto <nerat.neto@gmail.com>
2019-08-22 19:53:30 -0300

    new card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,38 @@
 <template>
 <div class="card">
+  <div v-if="title" class="title-container">
+    <p class="title">{{ title }}</p>
+  </div>
   <slot />
 </div>
 </template>
 
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+
 <style lang="scss" >
 .card {
+    background-color: #ffffff;
     border: 1px solid #dddddd;
     border-radius: 8px;
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
+    .title-container {
+        border-bottom: 1px solid #dddddd;
+        padding: 1vw;
+        .title {
+            margin: 0;
+            text-transform: uppercase;
+            color: #525252;
+            font-weight: 600;
+            font-size: 0.88em;
+        }
+    }
 }
 </style>
