d6e2a1625ead1d54e18f8e0cbd87853ec505e208
Neto <nerat.neto@gmail.com>
2019-08-21 21:44:47 -0300

    card and initial pages

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="card">
+  <slot />
+</div>
+</template>
+
+<style lang="scss" >
+.card {
+    border: 1px solid #dddddd;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+}
+</style>
---
+++ src/components/CompanyPage.vue
@@ -0,0 +1,19 @@
+<template>
+<card>
+  <p>company page</p>
+</card>
+</template>
+
+<script>
+import Card from './Card.vue'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  components: {
+    Card
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
