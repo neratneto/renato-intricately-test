1b8b5cc5dd6bad2edba8edd3c6a3d35b4590ece6
Neto <nerat.neto@gmail.com>
2019-08-22 19:54:11 -0300

    responsive image  component

---
+++ src/components/ResponsiveImage.vue
@@ -0,0 +1,16 @@
+<template>
+<img :src="image('')" :srcset="`${image('@2x')}, ${image('@3x')}`" />
+</template>
+
+<script>
+export default {
+  props: {
+    path: String
+  },
+  methods: {
+    image(resolution) {
+      return require(`../images/${this.path}${resolution}.png`)
+    }
+  }
+}
+</script>
