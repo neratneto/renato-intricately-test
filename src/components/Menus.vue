5f36a32f67c9e87a4b87aaf808520c74df598b32
Neto <nerat.neto@gmail.com>
2019-08-22 19:54:57 -0300

    altered tabs

--- src/components/Menus.vue
+++ src/components/Menus.vue
@@ -1,5 +1,5 @@
 <template>
-<card>
+<card class="tabs">
   <div :class="`tab ${tab === selectedTab && 'selected-tab'}`" v-for="(tab, index) in tabs" :key="index" @click="changeTab(tab)">{{ tab }}</div>
 </card>
 </template>
@@ -27,23 +27,28 @@ export default {
 </script>
 
 <style lang="scss">
-.tab {
-    color: #5D699A;
-    padding: 2vw;
-    font-weight: 600;
-    border-bottom: 1px solid #dddddd;
-    border-left: 4px solid #FFFFFF;
-}
-.tab:last-child {
-    border-bottom: none;
-    border-bottom-left-radius: 8px;
-}
-.tab:first-child {
-    border-top-left-radius: 8px;
-}
-.selected-tab {
-    background-color: #FAFBFC;
-    border-left: 4px solid #344279;
-    color: #525252;
+.tabs {
+    .tab {
+        color: #5D699A;
+        padding: 2vw;
+        font-weight: 600;
+        font-size: 1em;
+        border-bottom: 1px solid #dddddd;
+        border-left: 4px solid transparent;
+    }
+    .tab:last-child {
+        border-bottom: none;
+        border-bottom-left-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
+    .tab:first-child {
+        border-top-right-radius: 8px;
+        border-top-left-radius: 8px;
+    }
+    .selected-tab {
+        background-color: #FAFBFC;
+        border-left: 4px solid #344279;
+        color: #525252;
+    }
 }
 </style>
