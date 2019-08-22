0fc69e1787e386dff7b6697ec8669f226e104500
Neto <nerat.neto@gmail.com>
2019-08-21 21:44:29 -0300

    tabs and breadcrumbs

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,27 @@
+<template>
+<p class="breadcrumbs">HOME <span class="slash-margin">/</span> {{ selectedTab }}</p>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      selectedTab: state => state.selectedTab
+    })
+  }
+}
+</script>
+
+<style lang="scss" >
+.breadcrumbs {
+    color: #0000008f;
+    font-size: 0.92em;
+    font-weight: 500;
+    margin: 3vh 0;
+    .slash-margin {
+        margin: auto 1vw;
+    }
+}
+</style>
---
+++ src/components/Menus.vue
@@ -0,0 +1,49 @@
+<template>
+<card>
+  <div :class="`tab ${tab === selectedTab && 'selected-tab'}`" v-for="(tab, index) in tabs" :key="index" @click="changeTab(tab)">{{ tab }}</div>
+</card>
+</template>
+
+<script>
+import Card from './Card.vue'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  components: {
+    Card
+  },
+  computed: {
+    ...mapState({
+      selectedTab: state => state.selectedTab
+    })
+  },
+  methods: {
+    ...mapActions(['changeTab'])
+  },
+  data: () => ({
+    tabs: ['COMPANY DATA', 'COMPANY TABLE', 'COMPANY PAGE']
+  }),
+};
+</script>
+
+<style lang="scss">
+.tab {
+    color: #5D699A;
+    padding: 2vw;
+    font-weight: 600;
+    border-bottom: 1px solid #dddddd;
+    border-left: 4px solid #FFFFFF;
+}
+.tab:last-child {
+    border-bottom: none;
+    border-bottom-left-radius: 8px;
+}
+.tab:first-child {
+    border-top-left-radius: 8px;
+}
+.selected-tab {
+    background-color: #FAFBFC;
+    border-left: 4px solid #344279;
+    color: #525252;
+}
+</style>
