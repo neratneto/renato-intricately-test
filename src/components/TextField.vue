5d16cb0d5bb83b6feee3477dc7f780b8b8129e22
Neto <nerat.neto@gmail.com>
2019-08-23 19:05:23 -0300

    number validator

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,8 +1,7 @@
 <template>
 <div class="text-field">
   <span class="label">{{ label }}</span>
-  <textarea v-if="area" class="field area" rows="8" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" />
-  <input v-else class="field input" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)" />
+  <input class="field input" :placeholder="placeholder" :value="value" @input="updateValue" @blur="$emit('blur')" />
 </div>
 </template>
 
@@ -12,7 +11,21 @@ export default {
     value: [String, Number],
     label: String,
     placeholder: String,
-    area: Boolean
+    type: {
+      default: 'text',
+      type: String
+    }
+  },
+  methods: {
+    updateValue(event) {
+      let value = event.target.value
+      if (this.type === 'number') {
+        if (value.includes('$ ')) value = value.substring(1)
+        value = isNaN(Number(value)) ? null : Math.abs(value)
+      }
+      event.target.value = value
+      this.$emit('input', value)
+    }
   }
 }
 </script>
@@ -23,30 +36,8 @@ export default {
     flex-wrap: wrap;
     flex-direction: column;
     margin-top: 2vh;
-    .label {
-        color: #B1B1B1;
-        text-transform: uppercase;
-        font-weight: bold;
-        font-size: 0.88em;
-    }
-    .field {
-        margin: 0.8vh 0;
-        border: 1px solid #dadada;
-        border-radius: 3.2px;
-        box-shadow: 0 1px 8px rgba(0,0,0,0.08);
-        padding: 0.8vw;
-        font-size: 1.2em;
-        color: rgba(0,0,0,0.64);
-    }
-    .field::placeholder {
-        color: #d1d1d1;
-    }
     .input {
         max-width: 24vw;
-    }
-    .area {
-        font-family: 'Lato', sans-serif;
-        resize: none;
     }
 }
 </style>
